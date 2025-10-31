812c07136148d146f9192a1a1d0c91bc89d94be4
Nayane Oi <nayane_rms@hotmail.com>
2025-10-31 14:22:25 -0300

    Exemplos de Vetores

---
+++ aula04/ExemploVetor.js
@@ -0,0 +1,8 @@
+let vetorStrings = ["Boxer", "Pastor Alemão", "Pinscher", "Husky Siberiano", "Corgi"];
+
+for (let indice = 0; indice < 5; indice++) {
+
+    console.log(`${indice + 1}º elemento: ${vetorStrings[indice]}`);  // `` a crase pode ser usada para concatenar o texto com um variavel
+}
+
+console.log(vetorStrings.length) // para saber quantos vetores tem
